db219fd8e26dd3c9eee28a075b658e53623cb938
Yentzer Sensess <sensessyentzer@outlook.com>
2022-07-19 15:15:12 -0400

    Changed Files, added Type and Resolvers

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, { username, email, _id }, context) => {
+      
+      if (context.user) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: _id }, { username: username }],
+        });
+        return foundUser;
+      }
+      AuthenticationError("You are not logged in!")
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        AuthenticationError("Incorrect Credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        AuthenticationError("Incorrect Credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    saveBook: async (parent, { userInput, saveBookInput }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: userInput._id },
+          { $addToSet: { savedBooks: saveBookInput } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    deleteBook: async (parent, { userInput, bookId }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: userInput._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true, runValidators: true }
+        );
+        console.log(updatedUser);
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: String!
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input saveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+  }
+  input userInput {
+    _id: ID!
+    username: String!
+    email: String!
+  }
+  type Query {
+    me(username: String, email: String, _id: ID): User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(userInput: userInput!, saveBookInput: saveBookInput!): User
+    deleteBook(userInput: userInput!, bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
